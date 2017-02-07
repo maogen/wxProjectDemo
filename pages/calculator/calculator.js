f94889db2d3b44bd32f458cd779f9d7f72aabbec
maogen <614466874@qq.com>
2017-02-07 14:32:58 +0800

    实现计算器的加减乘除功能

--- pages/calculator/calculator.js
+++ pages/calculator/calculator.js
@@ -1,7 +1,11 @@
 // pages/calculator/calculator.js
 Page({
   data: {
-    resultNum: "0"
+    // 用来显示在屏幕上的数字
+    resultNum: "0",
+    // 当点击+-*/%=符号时时，进行运算时保存的数，以及运算符号id
+    numOne: "",
+    symbolId: ""
   },
   onLoad: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
@@ -33,7 +37,9 @@ Page({
   // 点击小数点的处理
   clickDot: function () {
     var _resultNum = this.data.resultNum;
-    _resultNum = _resultNum + ".";
+    if (_resultNum.indexOf(".") <= 0) {
+      _resultNum = _resultNum + ".";
+    }
     this.setData({ resultNum: _resultNum });
   },
   // 退格按钮处理
@@ -64,6 +70,48 @@ Page({
   },
   //点击符号按钮+-*/%=
   clickSymbol: function (item) {
-    // TODO
+    var _numOne = this.data.numOne;
+    var _resultNum = this.data.resultNum;
+    var _symbolId = this.data.symbolId;
+
+    var itemId = item.currentTarget.id;
+    console.log("上一个点击的符号：" + _symbolId);
+    console.log("本次点击的符号：" + itemId);
+
+    if ("result" != itemId) {
+      // +-*/%
+      // 保存第一个数
+      this.setData({ numOne: _resultNum });
+      // 保存运算符
+      this.setData({ symbolId: itemId });
+      // 重置显示结果
+      this.setData({ resultNum: "0" });
+    } else if ("" != _symbolId) {
+      // 求结果：运算符不为空
+      var result = 0.0;
+      if ("add" == _symbolId) {
+        result = parseFloat(_numOne) + parseFloat(_resultNum);
+      } else if ("sub" == _symbolId) {
+        result = parseFloat(_numOne) - parseFloat(_resultNum);
+      } else if ("mul" == _symbolId) {
+        result = parseFloat(_numOne) * parseFloat(_resultNum);
+      } else if ("div" == _symbolId) {
+        if (_resultNum == "0") {
+          result = "0";
+        } else {
+          result = parseFloat(_numOne) / parseFloat(_resultNum);
+        }
+      } else if ("rem" == _symbolId) {
+        if (_resultNum == "0") {
+          result = "0";
+        } else {
+          result = parseFloat(_numOne) % parseFloat(_resultNum);
+        }
+      }
+      // 将结果显示
+      this.setData({ resultNum: result });
+      // 将符号值为空
+      this.setData({ symbolId: "" });
+    }
   }
 })
